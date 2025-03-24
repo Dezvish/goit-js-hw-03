dd4190b5e02d1f977fc3adf7cb996cb6031e070e
Wisdom <dezvish@gmail.com>
2025-03-24 10:30:38 +0200

    hotfix-task3-and-2-swap

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,15 @@
-function makeArray(firstArray, secondArray, maxLength) {
-  let thirdArray = firstArray.concat(secondArray);
-  if (thirdArray.length >= maxLength) {
-    return thirdArray.slice(0, maxLength);
-  } else {
-    return thirdArray;
+function filterArray(numbers, value) {
+  let finalNumbers = [];
+  for (let i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] > value) {
+      finalNumbers.push(numbers[i]);
+    }
   }
+  return finalNumbers;
 }
 
-console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
-console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
-console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); // ["Mango", "Ajax", "Chelsea"]
-console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2)); // ["Earth", "Jupiter"]
-console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
-console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)); // []
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
